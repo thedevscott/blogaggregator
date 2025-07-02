d31004710670f924ada9a1fdb27afb1aac3f63a1
Omar Scott <thedevscott@gmail.com>
2025-07-01 21:30:30 -0400

    changed name data type

--- sql/schema/001_create_users.sql
+++ sql/schema/001_create_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL,
-    name VARCHAR(255) UNIQUE NOT NULL
+    name TEXT UNIQUE NOT NULL
 );
 
 -- +goose Down
